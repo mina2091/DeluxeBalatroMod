1b84e0e664d251f9f54d9e7744726c2ebc7abee2
atknoot <aerostatik_kk@protonmail.com>
2025-12-24 17:05:43 +0200

    added roffle joker (needs better image)

--- src/jokers/jokers.lua
+++ src/jokers/jokers.lua
@@ -40,116 +40,77 @@ SMODS.Atlas {
 }
 
 SMODS.Atlas({
-    key = "sample_wee",
-    path = "j_sample_wee.png",
+    key = "roffle",
+    path = "looksinside.png",
+    loc_txt = {
+        name = 'Double Exposure',
+        },
     px = 71,
-    py = 95
+    py = 95,
 })
 
 SMODS.Atlas({
-    key = "sample_obelisk",
+    key = "sj_sample_obelisk",
     path = "j_sample_obelisk.png",
     px = 71,
     py = 95
 })
 
 SMODS.Atlas({
-    key = "sample_specifichand",
+    key = "sj_sample_specifichand",
     path = "j_sample_specifichand.png",
     px = 71,
     py = 95
 })
 
 SMODS.Atlas({
-    key = "sample_money",
+    key = "sj_sample_money",
     path = "j_sample_money.png",
     px = 71,
     py = 95
 })
 
 SMODS.Atlas({
-    key = "sample_roomba",
+    key = "sj_sample_roomba",
     path = "j_sample_roomba.png",
     px = 71,
     py = 95
 })
 
 SMODS.Atlas({
-    key = "sample_drunk_juggler",
+    key = "sj_sample_drunk_juggler",
     path = "j_sample_drunk_juggler.png",
     px = 71,
     py = 95
 })
 
 SMODS.Atlas({
-    key = "sample_hackerman",
+    key = "sj_sample_hackerman",
     path = "j_sample_hackerman.png",
     px = 71,
     py = 95
 })
 
 SMODS.Atlas({
-    key = "sample_baroness",
+    key = "sj_sample_baroness",
     path = "j_sample_baroness.png",
     px = 71,
     py = 95
 })
 
 SMODS.Atlas({
-    key = "sample_rarebaseballcard",
+    key = "sj_sample_rarebaseballcard",
     path = "j_sample_rarebaseballcard.png",
     px = 71,
     py = 95
 })
 
 SMODS.Atlas({
-    key = "sample_multieffect",
+    key = "sj_sample_multieffect",
     path = "j_sample_multieffect.png",
     px = 71,
     py = 95
 })
-
-
-SMODS.Joker{
-    key = "sample_wee",                                  --name used by the joker.    
-    config = { extra = { chips = 8, chip_mod = 2 } },    --variables used for abilities and effects.
-    pos = { x = 0, y = 0 },                              --pos in spritesheet 0,0 for single sprites or the first sprite in the spritesheet.
-    rarity = 1,                                          --rarity 1=common, 2=uncommen, 3=rare, 4=legendary
-    cost = 1,                                            --cost to buy the joker in shops.
-    blueprint_compat=true,                               --does joker work with blueprint.
-    eternal_compat=true,                                 --can joker be eternal.
-    unlocked = true,                                     --is joker unlocked by default.
-    discovered = true,                                   --is joker discovered by default.    
-    effect=nil,                                          --you can specify an effect here eg. 'Mult'
-    soul_pos=nil,                                        --pos of a soul sprite.
-    atlas = 'sample_wee',                                --atlas name, single sprites are deprecated.
-
-    calculate = function(self,card,context)              --define calculate functions here
-        if context.individual and context.cardarea == G.play then -- if we are in card scoring phase, and we are on individual cards
-            if not context.blueprint then -- blueprint/brainstorm don't get to add chips to themselves
-                if context.other_card:get_id() == 2 then -- played card is a 2 by rank
-                    card.ability.extra.chips = card.ability.extra.chips + card.ability.extra.chip_mod -- add configurable amount of chips to joker
-                    
-                    return {                             -- shows a message under the specified card (card) when it triggers, k_upgrade_ex is a key in the localization files of Balatro
-                        extra = {focus = card, message = localize('k_upgrade_ex')},
-                        card = card,
-                        colour = G.C.CHIPS
-                    }
-                end
-            end
-        end
-        if context.joker_main and context.cardarea == G.jokers then
-            return {                                     -- returns total chips from joker to be used in scoring, no need to show message in joker_main phase, game does it for us.
-                chips = card.ability.extra.chips, 
-                colour = G.C.CHIPS
-            }
-        end
-    end,
-
-    loc_vars = function(self, info_queue, card)          --defines variables to use in the UI. you can use #1# for example to show the chips variable
-        return { vars = { card.ability.extra.chips, card.ability.extra.chip_mod }, key = self.key }
-    end
-}
 
 SMODS.Joker{
     key = "sample_obelisk",
@@ -162,7 +123,7 @@ SMODS.Joker{
     unlocked = true,
     discovered = true,
     effect = nil,
-    atlas = 'sample_obelisk',
+    atlas = 'sj_sample_obelisk',
     soul_pos = nil,
 
     calculate = function(self, card, context)
@@ -197,7 +158,7 @@ SMODS.Joker{
     discovered = true,
     effect=nil,
     soul_pos=nil,
-    atlas = 'sample_specifichand',
+    atlas = 'sj_sample_specifichand',
 
     calculate = function(self,card,context)
         if context.joker_main and context.cardarea == G.jokers then
@@ -227,7 +188,7 @@ SMODS.Joker{
     unlocked = true,
     discovered = true,
     effect = nil,
-    atlas = 'sample_money',
+    atlas = 'sj_sample_money',
     soul_pos = nil,
 
     calculate = function(self, card, context)
@@ -251,7 +212,7 @@ SMODS.Joker{
     unlocked = true,
     discovered = true,
     effect = nil,
-    atlas = 'sample_roomba',
+    atlas = 'sj_sample_roomba',
     soul_pos = nil,
 
         calculate = function(self, card, context)
@@ -293,7 +254,7 @@ SMODS.Joker{
     unlocked = true,
     discovered = true,
     effect = nil,
-    atlas = 'sample_drunk_juggler',
+    atlas = 'sj_sample_drunk_juggler',
     soul_pos = nil,
 
     calculate = function(self, card, context)
@@ -316,7 +277,7 @@ SMODS.Joker{
     unlocked = true,
     discovered = true,
     effect = nil,
-    atlas = 'sample_hackerman',
+    atlas = 'sj_sample_hackerman',
     soul_pos = nil,
 
     calculate = function(self, card, context)
@@ -349,7 +310,7 @@ SMODS.Joker{
     unlocked = true,
     discovered = true,
     effect = nil,
-    atlas = 'sample_baroness',
+    atlas = 'sj_sample_baroness',
     soul_pos = nil,
 
     calculate = function(self, card, context)
@@ -387,7 +348,7 @@ SMODS.Joker{
     unlocked = true,
     discovered = true,
     effect = nil,
-    atlas = 'sample_rarebaseballcard',
+    atlas = 'sj_sample_rarebaseballcard',
     soul_pos = nil,
 
     calculate = function(self, card, context)
@@ -417,7 +378,7 @@ SMODS.Joker{
     unlocked = true,
     discovered = true,
     effect = nil,
-    atlas = 'sample_multieffect',
+    atlas = 'sj_sample_multieffect',
     soul_pos = nil,
 
     calculate = function(self, card, context)
@@ -470,3 +431,41 @@ end
         end
     end
 }
+
+SMODS.Joker {
+    key = "roffle",
+    pos = { x = 0, y = 0 },
+    rarity = 2,
+    blueprint_compat = false,
+    cost = 8,
+    discovered = true,
+    atlas = "roffle",
+    loc_txt = {
+        name = "Roffle",
+        text = {
+            "{C:dark_edition}Looks Inside{}",
+        }
+    },
+      calculate = function(self, card, context)
+        if context.selling_self then
+context.cardarea = G.jokers
+	context.elling_card = true
+    card = card
+            return {
+                func = function()
+    G.E_MANAGER:add_event(Event({
+        func = function()
+            local keys = {'j_photograph', 'j_hanging_chad'}
+            SMODS.add_card{ key = "j_hanging_chad", edition = { negative = true } }
+            SMODS.add_card{ key = "j_photograph", edition = { negative = true } }
+            return true 
+        end
+    }))
+end
+            }
+        end
+    end 
+}
+
+
+
